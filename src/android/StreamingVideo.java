a7423af908678680d0522a0073f53819cf4f05e0
Malte Köhrer <mk@notboring.org>
2020-08-19 19:52:56 +0200

    Update StreamingVideo.java
    use immersive mode

--- src/android/StreamingVideo.java
+++ src/android/StreamingVideo.java
@@ -59,7 +59,14 @@ public class StreamingVideo extends Activity implements
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    View decorView = getWindow().getDecorView();
+    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
     // Create an AdsLoader.
     mSdkFactory = ImaSdkFactory.getInstance();
